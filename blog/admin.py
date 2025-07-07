3ea6e554963691fd7b3c85ac2a2cd94d42bb0b58
Snehal Shelar <101625430+snehals21@users.noreply.github.com>
2025-07-07 16:20:52 +0530

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from . import models
 from mptt.admin import MPTTModelAdmin
 
 
-admin.site.registermodels.Book)
+admin.site.register(models.Book)
 
 # following is the MPTT model registration.
 admin.site.register(models.Chapter, MPTTModelAdmin)
